f220e1f2c217aa440a952beb3f9b6b3443c227dd
SergeyEgorkin <deliv88@mail.ru>
2021-03-08 21:28:56 +0500

    Complete lesson 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Главная'
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    location = [
+        {'city': 'Москва', 'phone': '+7-888-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
+        {'city': 'Москва', 'phone': '+7-888-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
+        {'city': 'Москва', 'phone': '+7-888-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
+    ]
+    content = {
+        'title': 'Контакты',
+        'location': location
+    }
+    return render(request, 'mainapp/contact.html', content)
